b0d8638e69c866eeaab4fa5daedffc31feed3724
Lee <inbox@leefallat.ca>
2021-02-06 15:29:38 -0500

    Work in chats with gradient backward

--- contentScript.js
+++ contentScript.js
@@ -1,13 +1,25 @@
-const profilePictureHref = document.querySelector('svg').querySelector('image').href.baseVal;
-var style = document.createElement('style');
-style.type = 'text/css';
-style.innerHTML = `[data-testid="outgoing_group"] > [role="row"]:last-of-type::before {
-    width: 28px;
-    height: 28px;
-    margin: 0 8px;
-    background-image: url('${profilePictureHref}');
-    background-position: center;
-    background-size: cover;
-    border-radius: 100%;
-  }`;
-document.getElementsByTagName('head')[0].appendChild(style);
+document.addEventListener('DOMContentLoaded', () => {
+  getPic();
+});
+
+function getPic() {
+  return setTimeout(() => {
+    const svg = document.querySelector('svg');
+    if (!svg) { return getPic(); }
+    
+    const profilePictureHref = svg.querySelector('image').href.baseVal;
+    
+    var style = document.createElement('style');
+    style.type = 'text/css';
+    style.innerHTML = `[data-testid="outgoing_group"] > [role="row"]:last-of-type::before {
+        width: 28px;
+        height: 28px;
+        margin: 0 8px;
+        background-image: url('${profilePictureHref}');
+        background-position: center;
+        background-size: cover;
+        border-radius: 100%;
+      }`;
+    document.getElementsByTagName('head')[0].appendChild(style);
+  }, 1);
+}
